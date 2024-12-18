1a608bf3238e60dd8f4c6ecdcd8a60211e2ed54c
Shihab k s <neethu@luminartechnolab.com>
2024-12-18 15:37:12 +0530

    embeded js

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -6,20 +6,30 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 const d = new Date();
-let day = d.getDay()
+let day = d.getDay();
+let dayToPass = "Weekend";
+let quote = "Its time to have fun";
 
 app.get("/", (req, res) => {
 
 
-    if (day == 6 || day == 7) {
-        res.render(__dirname + "/views/index.ejs",
-            { day: `Weekend`, quote: `Its time to have fun` }
-        );
+
+    // 0- sunday
+    //7 - saturday 
+    if (day == 0 || day == 7) {
+        dayToPass = "Weekend";
+        quote = "Its time to have fun";
+
     } else {
-        res.render(__dirname + "/views/index.ejs",
-            { day: `Weekday`, quote: `Its time to work Hard` }
-        );
+
+        dayToPass = "Weekday";
+        quote = "Its time to work Hard";
+
     }
+
+    res.render(__dirname + "/views/index.ejs",
+        { day: dayToPass, quote: quote }
+    );
 
 
 });
