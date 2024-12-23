d1c10f1291a6026f40cce783cfaabeafda8449d3
shihabksaleem <Shihab.ks56@gmail.com>
2024-12-24 00:48:27 +0530

    added npm

--- Backend/4.2+Passing+Data/index.js
+++ Backend/4.2+Passing+Data/index.js
@@ -6,9 +6,13 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
 
-app.post("/submit", (req, res) => {});
+});
+
+app.post("/submit", (req, res) => {
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
